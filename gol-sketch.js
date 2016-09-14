95662ffce4d7e38ac20c91ae39cd9c6da9ad993a
Rodrigo M. Cucick <r_monfredini@hotmail.com>
2016-09-14 11:23:04 -0300

    Added comments.

--- gol-sketch.js
+++ gol-sketch.js
@@ -46,7 +46,7 @@ function setup() {
  */
 function draw() {
 	if (!isPaused) {
-		// If you have a high frequency CPU and/or GPU, it's possible that you'll need to adjust the way
+		// If you have a high frequency CPU and/or GPU it's possible that you'll need to adjust the way
 		// the timer variable works, so the simulation will run at a reasonable pace, not too fast, not too slow.
 		// There's nothing "fancy" here, it's just a basic cycle counter. Using floats here might be a good a idea.
 		if (timer % 2 == 0) {
